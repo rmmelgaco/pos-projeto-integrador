9313bfb5deae2929c703a4bb91ee2f803ce2d8cd
melgacorm <rmmelgaco@gmail.com>
2024-07-10 14:19:50 -0300

    unidade 3 video 1

--- src/templates/user-template/index.tsx
+++ src/templates/user-template/index.tsx
@@ -1,10 +1,17 @@
 import Header from "../../components/header";
 import Footer from "../../components/footer";
-import {PropsWithChildren} from "react";
+import {PropsWithChildren, useEffect} from "react";
+import {useLocation} from "react-router-dom";
 
 type UserTemplateProps = PropsWithChildren
 
 export default function UserTemplate(props: UserTemplateProps) {
+    const {pathname} = useLocation()
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [pathname])
+
     return (
         <div className='min-h-screen flex flex-col bg-gray-100'>
             <Header/>
